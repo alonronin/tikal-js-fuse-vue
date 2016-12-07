639748816509f7d38447bf94be08bb398324b8d3
Izhar Gavish <izhar@tikalk.com>
2016-12-07 13:56:40 +0200

    Format articles table values

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,8 +1,11 @@
 <template>
     <el-table :data="articles" border style="width: 100%">
-      <el-table-column prop="published" label="Date" sortable width="180"></el-table-column>
+      <el-table-column prop="published" label="Date" :formatter="dateFormatter" sortable width="180"></el-table-column>
       <el-table-column prop="author" label="Author" width="180"></el-table-column>
-      <el-table-column prop="title" label="Title">
+      <el-table-column inline-template label="Title">
+        <div>
+          <a :href="row.url" target="_blank">{{ row.title }}</a>
+        </div>
       </el-table-column>
     </el-table>
 </template>
@@ -15,6 +18,11 @@ export default {
     return {
       articles: this.articleList
     }
+  },
+  methods: {
+   dateFormatter(row, column) {
+     return (new Date(Date.parse(row.published)).toDateString());
+   }
   }
 }
 </script>
