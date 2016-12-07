c448bffc94d7ca707579626a903189d4b5039a00
Izhar Gavish <izhar@tikalk.com>
2016-12-07 14:28:26 +0200

    Minor fixes

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-table :data="articles" border style="width: 100%">
+    <el-table stripe :data="articles" border style="width: 100%">
       <el-table-column prop="published" label="Date" :formatter="dateFormatter" sortable width="180"></el-table-column>
       <el-table-column prop="author" label="Author" width="180"></el-table-column>
       <el-table-column inline-template label="Title">
