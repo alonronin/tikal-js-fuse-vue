165afc35ecc28ff72b3994e8c25fd96c0188c381
Alon Valadji <alon@ronin.co.il>
2016-12-07 11:23:46 +0200

    install element css framework

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <el-menu theme="dark" default-active="1" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1">Processing Center</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">Workspace</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="3">Orders</el-menu-item>
+    </el-menu>
+    <h2>{{ count }}</h2>
     <ul>
-      <li v-for="article in articles">{{ article | json}}</li>
+      <li v-for="article in articles">
+        <pre>{{ article.author }}</pre>
+      </li>
     </ul>
     <hello></hello>
   </div>
 </template>
 
 <script>
+import 'element-ui/lib/theme-default/index.css';
 import firebase from 'firebase'
 import Hello from './components/Hello'
 
@@ -28,6 +41,11 @@ export default {
   firebase: {
     articles: db.ref('/stream')
   },
+  computed: {
+    count() {
+      return this.articles.length
+    }
+  },
   components: {
     Hello
   }
@@ -36,11 +54,5 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
